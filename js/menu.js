939548de8e5d4d1d7ebd0ef75fdf02e5a7c4e3b0
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-05-31 15:12:24 -0300

    navigation and navigation functionality

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+const items = document.querySelectorAll('nav ul li a')
+let clicked;
+
+function click(element){
+    while(true){
+        if(clicked == undefined){ // if the element is not clicked
+            element.style.backgroundColor = '#0000003a'
+            clicked = element
+            break
+        }else{ // if it is clicked, clears the markup
+            clicked.style.backgroundColor = 'inherit'
+            clicked = undefined
+        }        
+    }
+}
+document.addEventListener('click', function(local){
+    if(local.path[0].localName == 'a'){ // if the local where it was clicked is an 'a'
+        for(e in items){
+            if(items[e].innerText == local.path[0].innerText){ // if the text inside that 'a' is the same as in the html
+                click(items[e])
+            }
+        }
+    }
+})
